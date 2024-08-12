b8325252714901576b021a87a0dc02f244dee7ff
Basavarajy14 <razzzofficial@gmail.com>
2024-08-12 23:16:19 +0530

    Initial commit

---
+++ Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class FurnitureManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    GameObject indicatorToggleButton, pointerIndicatorObj;
+    GameObject indicatorObj, currentFurnitureObj;
+    public List<GameObject> furnitureList = new List<GameObject>();
+
+    void Awake()
+    {
+        if(indicatorToggleButton.activeInHierarchy)
+        {
+            indicatorToggleButton.SetActive(false);
+        }
+        indicatorObj = pointerIndicatorObj.transform.GetChild(0).gameObject;
+    }
+
+
+    public void FurnitureToggleButton(int index)
+    {
+
+        if(!furnitureList[index].activeInHierarchy)
+        {
+            SpawnFurniture(index);
+        }
+        else
+        {
+            DisableFurniture(index);
+        }
+
+
+
+
+
+    }
+
+    //private void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        currentFurnitureObj = furnitureList[2];
+    //        currentFurnitureObj.transform.position = pointerIndicatorObj.transform.position;
+    //        currentFurnitureObj.transform.rotation = pointerIndicatorObj.transform.rotation;
+    //        currentFurnitureObj.SetActive(true);
+
+           
+
+    //    }
+
+    //}
+
+
+    private void SpawnFurniture(int index)
+    {
+        if(index>=0&&index<furnitureList.Count)
+            currentFurnitureObj = furnitureList[index];
+
+        if (currentFurnitureObj != null && indicatorObj.activeInHierarchy)
+        {
+            currentFurnitureObj.transform.position = pointerIndicatorObj.transform.position;
+            currentFurnitureObj.transform.rotation = pointerIndicatorObj.transform.rotation;
+            currentFurnitureObj.SetActive(true);
+
+            if(!indicatorToggleButton.activeInHierarchy)
+                indicatorToggleButton.SetActive(true);
+
+
+        }
+    }
+
+    private void DisableFurniture(int index)
+    {
+        if(furnitureList[index].activeInHierarchy)
+            furnitureList[index].SetActive(false);
+
+    }
+
+    public void IndicatorToggleButton()
+    {
+        if(pointerIndicatorObj.activeInHierarchy)
+        {
+            pointerIndicatorObj.SetActive(false);
+            indicatorObj.SetActive(false);
+        }else
+        {
+            pointerIndicatorObj.SetActive(true);
+            indicatorObj.SetActive(true);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+
+    private ARRaycastManager rayManager;
+    private GameObject indicatorObj;
+
+    void Start()
+    {
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        indicatorObj = this.transform.GetChild(0).gameObject;
+        indicatorObj.SetActive(false);
+       
+    }
+
+    void Update()
+    {
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // if we hit an AR plane surface, update the position and rotation
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            var CameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(CameraForward.x, 0f, CameraForward.z).normalized;
+            transform.rotation = Quaternion.LookRotation(cameraBearing);
+
+
+            //transform.rotation = hits[0].pose.rotation;
+
+            if (!indicatorObj.activeInHierarchy)
+            {
+                indicatorObj.SetActive(true);
+            }
+
+            
+        }
+    }
+}
